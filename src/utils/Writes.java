52c816c20d782b3cec22f38ad3085280b7830cae
Patrick Clark <patrickwclark12@gmail.com>
2021-11-17 19:04:34 -0600

    That should do it, but there's probably still a conflict or something.

--- src/utils/Writes.java
+++ src/utils/Writes.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import main.ArrayVisualizer;
 
 /*
- * 
+ *
 MIT License
 
 Copyright (c) 2019 w0rthy
@@ -45,14 +45,14 @@ final public class Writes {
     private volatile long auxWrites;
     private volatile long writes;
     private volatile long allocAmount;
-    
+
     private DecimalFormat formatter;
-    
+
     private ArrayVisualizer ArrayVisualizer;
     private Delays Delays;
     private Highlights Highlights;
     private Timer Timer;
-    
+
     public Writes(ArrayVisualizer ArrayVisualizer) {
         this.reversals = 0;
         this.depth = 0;
@@ -60,15 +60,15 @@ final public class Writes {
         this.auxWrites = 0;
         this.writes = 0;
         this.allocAmount = 0;
-        
+
         this.ArrayVisualizer = ArrayVisualizer;
         this.Delays = ArrayVisualizer.getDelays();
         this.Highlights = ArrayVisualizer.getHighlights();
         this.Timer = ArrayVisualizer.getTimer();
-        
+
         this.formatter = ArrayVisualizer.getNumberFormat();
     }
-    
+
     public void resetStatistics() {
         this.recursions = 0;
         this.depth = 0;
@@ -78,7 +78,7 @@ final public class Writes {
         this.writes = 0;
         this.allocAmount = 0;
     }
-    
+
     public String getReversals() {
         if(this.reversals < 0) {
             this.reversals = Long.MIN_VALUE;
@@ -122,7 +122,7 @@ final public class Writes {
             else                return this.formatter.format(this.swaps) + " Swaps";
         }
     }
-    
+
     public String getAuxWrites() {
         if(this.auxWrites < 0) {
             this.auxWrites = Long.MIN_VALUE;
@@ -133,7 +133,7 @@ final public class Writes {
             else                     return this.formatter.format(this.auxWrites) + " Writes to Auxiliary Array(s)";
         }
     }
-    
+
     public String getMainWrites() {
         if(this.writes < 0) {
             this.writes = Long.MIN_VALUE;
@@ -155,11 +155,11 @@ final public class Writes {
             else                      return this.formatter.format(this.allocAmount) + " Items in External Arrays";
         }
     }
-    
+
     public void changeAuxWrites(int value) {
         this.auxWrites += value;
     }
-    
+
     public void changeWrites(int value) {
         this.writes += value;
     }
@@ -171,11 +171,11 @@ final public class Writes {
     public void clearAllocAmount() {
         this.allocAmount = 0;
     }
-    
+
     public void changeReversals(int value) {
         this.reversals += value;
     }
-    
+
     private void updateSwap(boolean auxwrite) {
         this.swaps++;
         if(auxwrite) this.auxWrites += 2;
@@ -214,7 +214,7 @@ final public class Writes {
             ArrayVisualizer.updateNow();
             Delays.sleep(pause);
             
-            }
+        }
     }
 
     public void multiSwap(int[] array, int pos, int to, double sleep, boolean mark, boolean auxwrite) {
@@ -231,10 +231,10 @@ final public class Writes {
             }
         }
     }
-    
+
     public void reversal(int[] array, int start, int length, double sleep, boolean mark, boolean auxwrite) {
         this.reversals++;
-        
+
         for(int i = start; i < start + ((length - start + 1) / 2); i++) {
             this.swap(array, i, start + length - i, sleep, mark, auxwrite);
         }
@@ -259,16 +259,16 @@ final public class Writes {
         }
 
         if(mark) Highlights.markArray(1, at);
-        
+
         if(auxwrite) auxWrites++;
         else            writes++;
-        
+
         Timer.startLap("Write");
-        
+
         array[at] = equals;
 
         Timer.stopLap();
-        
+
         ArrayVisualizer.updateNow();
         Delays.sleep(pause);
     }
@@ -276,15 +276,15 @@ final public class Writes {
     public <T> void write(T[] array, int at, T equals, double pause, boolean mark) {
         if (ArrayVisualizer.sortCanceled()) throw new StopSort();
         if(mark) Highlights.markArray(1, at);
-        
+
         auxWrites++;
-        
+
         Timer.startLap("Write");
-        
+
         array[at] = equals;
 
         Timer.stopLap();
-        
+
         ArrayVisualizer.updateNow();
         Delays.sleep(pause);
     }
@@ -300,16 +300,16 @@ final public class Writes {
     public void multiDimWrite(int[][] array, int x, int y, int equals, double pause, boolean mark, boolean auxwrite) {
         if (ArrayVisualizer.sortCanceled()) throw new StopSort();
         if(mark) Highlights.markArray(1, x);
-        
+
         if(auxwrite) auxWrites++;
         else            writes++;
 
         Timer.startLap();
-        
+
         array[x][y] = equals;
 
         Timer.stopLap();
-        
+
         ArrayVisualizer.updateNow();
         Delays.sleep(pause);
     }
@@ -317,15 +317,15 @@ final public class Writes {
     public <T> void multiDimWrite(T[][] array, int x, int y, T equals, double pause, boolean mark) {
         if (ArrayVisualizer.sortCanceled()) throw new StopSort();
         if(mark) Highlights.markArray(1, x);
-        
+
         auxWrites++;
 
         Timer.startLap();
-        
+
         array[x][y] = equals;
 
         Timer.stopLap();
-        
+
         ArrayVisualizer.updateNow();
         Delays.sleep(pause);
     }
@@ -334,15 +334,15 @@ final public class Writes {
     public void mockWrite(int length, int pos, int val, double pause) {
         if (ArrayVisualizer.sortCanceled()) throw new StopSort();
         int[] mockArray = new int[length];
-        
+
         this.auxWrites++;
 
         Timer.startLap();
-        
+
         mockArray[pos] = val;
 
         Timer.stopLap();
-        
+
         Delays.sleep(pause);
     }
 
@@ -365,7 +365,7 @@ final public class Writes {
         for(ArrayList<Integer> list : registers) {
             total += list.size();
         }
-        
+
         for(int index = registers.length - 1; index >= 0; index--) {
             for(int i = registers[index].size() - 1; i >= 0; i--) {
                 this.write(array, total + min - temp++ - 1, registers[index].get(i), 0, mark, auxwrite);
@@ -385,12 +385,12 @@ final public class Writes {
         for(int i = 0; i < length; i++) {
             int register = i % radix;
             int pos = (register * (length / radix)) + (i / radix);
-            
+
             if(!tempWrite[pos]) {
                 this.write(array, pos, tempArray[pos], 0, false, false);
                 tempWrite[pos] = true;
             }
-            
+
             Highlights.markArray(register, pos);
             if(register == 0) Delays.sleep(sleep);
         }
@@ -399,22 +399,36 @@ final public class Writes {
                 this.write(array, i, tempArray[i], 0, false, false);
             }
         }
-    
+
         Highlights.clearAllMarks();
 
         this.deleteExternalArray(tempArray);
     }
-    
-    //Methods mocking System.arraycopy (reversearraycopy is for TimSort's MergeHi and BinaryInsert, and WikiSort's Rotate)
+
+    /**
+     * Method to mimic {@link System#arraycopy(Object, int, Object, int, int)}
+     * @see System#arraycopy(Object, int, Object, int, int)
+     */
     public void arraycopy(int[] src, int srcPos, int[] dest, int destPos, int length, double sleep, boolean mark, boolean aux) {
-        for(int i = 0; i < length; i++) {
-            if(mark) {
-                if(aux) Highlights.markArray(1, srcPos  + i);
-                else    Highlights.markArray(1, destPos + i);
+        int start, end, dir;
+        if (src != dest || destPos < srcPos) {
+            start = 0;
+            end = length;
+            dir = 1;
+        } else {
+            start = length - 1;
+            end = -1;
+            dir = -1;
+        }
+        for (int i = start; i != end; i += dir) {
+            if (mark) {
+                if (aux) {
+                    Highlights.markArray(1, srcPos + i);
+                } else {
+                    Highlights.markArray(1, destPos + i);
+                }
             }
-            
-            //TODO: Handle order of Delays in write method better
-            this.write(dest, destPos + i, src[srcPos + i], sleep, false, aux);
+            write(dest, destPos + i, src[srcPos + i], sleep, false, aux);
         }
     }
 
@@ -433,16 +447,15 @@ final public class Writes {
         ArrayVisualizer.updateNow();
         return result;
     }
-    
+
+    /**
+     * Same as arraycopy, but used to copy in reverse
+     * @deprecated Use {@link Writes#arraycopy(int[], int, int[], int, int, double, boolean, boolean)} instead
+     * @see Writes#arraycopy(int[], int, int[], int, int, double, boolean, boolean)
+     */
+    @Deprecated
     public void reversearraycopy(int[] src, int srcPos, int[] dest, int destPos, int length, double sleep, boolean mark, boolean aux) {
-        for(int i = length - 1; i >= 0; i--) {
-            if(mark) {
-                if(aux) Highlights.markArray(1, srcPos  + i);
-                else    Highlights.markArray(1, destPos + i);
-            }
-            
-            this.write(dest, destPos + i, src[srcPos + i], sleep, false, aux);
-        }
+        arraycopy(src, srcPos, dest, destPos, length, sleep, mark, aux);
     }
 
     public int[] createExternalArray(int length) {
@@ -501,20 +514,20 @@ final public class Writes {
             deleteArrayList(aList);
         }
     }
-    
+
     //TODO: These methods should be solely controlled by Timer class
     public void addTime(long milliseconds) {
         if(Timer.timerEnabled()) Timer.manualAddTime(milliseconds);
     }
-    
+
     public void setTime(long milliseconds) {
         if(Timer.timerEnabled()) Timer.manualSetTime(milliseconds);
     }
-    
+
     public void startLap() {
         if(Timer.timerEnabled()) Timer.startLap();
     }
-    
+
     public void stopLap() {
         if(Timer.timerEnabled()) Timer.stopLap();
     }
